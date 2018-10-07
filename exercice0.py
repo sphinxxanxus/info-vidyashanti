19f9844bf96dafaf2b18139ae937042e83a07631
EagleCorp <eagle@localhost.localdomain>
2018-10-07 03:18:34 +0200

    End of exercice0

---
+++ exercice0.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*
+
+print("I'm Léonnel KAKPO member of ceceonat")
+print("Email: atchenik05@gmail.com ")
+print("Logins:\n \t GitHub: vidyashanti\n \t GitLab: EagleWay")
+print("\t Programming languages: Python, C++")
